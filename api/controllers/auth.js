7edd6fcfcf6d465c5a665141cba36ecef19bb993
fbarrera29 <fbarrera29dev@gmail.com>
2023-01-06 19:39:23 +0100

    Endend testing back-end

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -20,8 +20,7 @@ const _login = async (req, res) => {
 
 const _checkToken = async (req, res) => {
   try {
-    console.log(req.headers.authorization.slice(7))
-    jwt.decode(req.headers.authorization.slice(7), jwt_secret)
+    jwt.decode(req.headers.authorization, jwt_secret)
     return successResponse({ data: 'ok', res })
   } catch (err) {
     return errorResponse({ statusCode: 500, err, res })
--- api/controllers/transaction.js
+++ api/controllers/transaction.js
@@ -4,6 +4,7 @@ import { Model } from 'objection'
 import Addresses from '../models/addresses.js'
 import Sections from '../models/sections.js'
 import Users from '../models/users.js'
+import Users_new from '../models/users_new.js'
 import { errorResponse, successResponse } from '../utils/responseHelper.js'
 
 const addAddress = async (trx) => {
@@ -12,13 +13,13 @@ const addAddress = async (trx) => {
   })
 }
 
-const addSections = async (trx) => {
+const addSections = async (trx, users_new_id) => {
   //end this part of code (use this function to add section)
   return await Sections.query(trx).insert({
-    users_new_id: 1,
+    users_new_id: users_new_id,
     name: 'inserted section',
-    description: { type: 'string', minLength: 1, maxLength: 255 },
-    type: { type: 'string', minLength: 1, maxLength: 255 },
+    description: 'some description',
+    type: 'an example of type',
   })
 }
 
@@ -34,15 +35,27 @@ const addUser = async (trx, addressId) => {
   })
 }
 
+const addUser_new = async (trx) =>{
+  return await Users_new.query(trx).insert({
+    name: 'name',
+    surname: 'surname',
+    pwd_hash: 'testing_insert_pwd',
+    admin: true
+  })
+}
+
 const _transaction = async (req, res) => {
   let trx
   try {
     trx = await Model.startTransaction()
     const address = await addAddress(trx)
     console.log('add address:', address)
-    console.log(address.id)
+    console.log(typeof address.id)
     const user = await addUser(trx, address.id)
     console.log('add user:', user)
+    const users_new = await addUser_new(trx);
+    const section = await addSections(trx, users_new.id);
+    console.log('added section with id ', section.id);
     return successResponse({ data: user, res, trx })
   } catch (err) {
     return errorResponse({ err, res, trx })
